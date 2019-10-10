08612989e669ed0d5f1af7b1983f2834189e1e47
Robin Ole Heinemann <robin.ole.heinemann@t-online.de>
2019-10-10 17:18:08 +0200

    add iter

--- btree/src/lib.rs
+++ btree/src/lib.rs
@@ -2,7 +2,7 @@
 
 use core::fmt::Debug;
 
-trait BTreeProxy<V: PartialOrd, I: BTreeIdx> {
+pub trait BTreeProxy<V: PartialOrd, I: BTreeIdx> {
     fn root(&self) -> I;
     fn set_root(&mut self, node: I);
 
@@ -21,7 +21,7 @@ trait BTreeProxy<V: PartialOrd, I: BTreeIdx> {
     fn value(&self, node: I) -> V;
 }
 
-trait BTreeIdx: Copy + PartialEq {
+pub trait BTreeIdx: Copy + PartialEq {
     fn is_none(&self) -> bool;
     fn is_some(&self) -> bool {
         !self.is_none()
@@ -30,9 +30,18 @@ trait BTreeIdx: Copy + PartialEq {
     fn none() -> Self;
 }
 
-trait BTree<V, I> {
+pub trait BTree<'a, V, I, DFIter: Iterator<Item = (I, V)>> {
+    fn df_iter_from(&'a self, idx: I) -> DFIter;
+    fn df_iter(&'a self) -> DFIter;
+
+    
+    fn into_df_iter_from(self, idx: I) -> DFIter;
+    fn into_df_iter(self) -> DFIter;
+
+    /*
     fn df_map_from<T, F: FnMut(I, V) -> Option<T>>(&self, idx: I, func: &mut F) -> Option<T>;
     fn df_map<T, F: FnMut(I, V) -> Option<T>>(&self, func: &mut F) -> Option<T>;
+    */
 
     fn first(&self) -> I;
     fn last(&self) -> I;
@@ -87,281 +96,314 @@ fn btree_df_map_from<V: PartialOrd, I: BTreeIdx, P: BTreeProxy<V, I>, T, F: FnMu
 }
 */
 
-impl<V: PartialOrd + Debug, I: BTreeIdx + Debug, P: BTreeProxy<V, I>> BTree<V, I> for P {
-fn max_depth_from(&self, node: I) -> usize {
-    use core::cmp::max;
 
-    if node.is_some() {
-        1 + max(self.max_depth_from(self.get_left(node)), self.max_depth_from(self.get_right(node)))
-    } else {
-        0
+enum BTreeDFIterState {
+    GoingLeft,
+    GoingRightOrUp
+}
+
+struct BTreeDFIter<'a, V: PartialOrd, I: BTreeIdx, P: BTreeProxy<V, I>> {
+    node: I,
+    done_left: bool,
+    state: BTreeDFIterState,
+    proxy: &'a P,
+    _marker: core::marker::PhantomData<V>
+}
+
+impl<'a, V: PartialOrd, I: BTreeIdx, P: BTreeProxy<V, I>> BTreeDFIter<'a, V, I, P> {
+    fn new(proxy: &'a P, idx: I) -> Self {
+        BTreeDFIter {
+            node: idx,
+            done_left: false,
+            state: BTreeDFIterState::GoingLeft,
+            proxy: proxy,
+            _marker: core::marker::PhantomData,
+        }
     }
 }
 
-fn max_depth(&self) -> usize {
-    self.max_depth_from(self.root())
-}
+impl<'a, V: PartialOrd, I: BTreeIdx, P: BTreeProxy<V, I>> Iterator for BTreeDFIter<'a, V, I, P> {
+    type Item = (I, V);
 
-fn df_map_from<T, F: FnMut(I, V) -> Option<T>>(&self, idx: I, func: &mut F) -> Option<T> {
-    let mut node = idx;
-    let mut done_left = false;
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.state {
+            BTreeDFIterState::GoingLeft => {
+                self.state = BTreeDFIterState::GoingRightOrUp;
 
-    loop {
-        let mut left = self.get_left(node);
+                let mut left = self.proxy.get_left(self.node);
 
-        if !done_left {
-            while left.is_some() {
-                node = left;
-                left = self.get_left(node);
-            }
+                if !self.done_left {
+                    while left.is_some() {
+                        self.node = left;
+                        left = self.proxy.get_left(self.node);
+                    }
 
-            let ret = func(node, self.value(node));
-            if ret.is_some() {
-                return ret;
-            }
+                    self.done_left = true;
 
-            done_left = true;
-        }
-
-        let right = self.get_right(node);
-
-        if right.is_some() {
-            node = right;
-            done_left = false;
-
-        } else {
-            let mut parent = self.get_parent(node);
-
-            if parent.is_some() {
-                while parent.is_some() && (node != self.get_left(parent)) {
-                    node = parent;
-                    
-                    parent = self.get_parent(node);
+                    Some((self.node, self.proxy.value(self.node)))
+                } else {
+                    self.next()
                 }
+            },
+            BTreeDFIterState::GoingRightOrUp => {
+                self.state = BTreeDFIterState::GoingLeft;
 
-                node = parent;
+                let right = self.proxy.get_right(self.node);
 
-                if node.is_none() {
-                    break;
+                if right.is_some() {
+                    self.node = right;
+                    self.done_left = false;
+
+                    self.next()
+                } else {
+                    let mut parent = self.proxy.get_parent(self.node);
+
+                    if parent.is_some() {
+                        while parent.is_some() && (self.node != self.proxy.get_left(parent)) {
+                            self.node = parent;
+
+                            parent = self.proxy.get_parent(self.node);
+                        }
+
+                        self.node = parent;
+
+                        if self.node.is_none() {
+                            None
+                        } else {
+                            Some((self.node, self.proxy.value(self.node)))
+                        }
+                    } else {
+                        None
+                    }
                 }
-
-                let ret = func(node, self.value(node));
-                if ret.is_some() {
-                    return ret;
-                }
-            } else {
-                break;
             }
         }
     }
-
-    None
 }
 
-fn df_map<T, F: FnMut(I, V) -> Option<T>>(&self, func: &mut F) -> Option<T> {
-    self.df_map_from(self.root(), func)
-}
+impl<'a, V: PartialOrd + Debug, I: BTreeIdx + Debug, P: BTreeProxy<V, I>> BTree<'a, V, I, BTreeDFIter<'a, V, I, P>> for P {
+    fn max_depth_from(&self, node: I) -> usize {
+        use core::cmp::max;
 
-fn first(&self) -> I {
-    let mut cur = self.root();
-    
-    loop {
-        let left = self.get_left(cur);
-
-        if left.is_some() {
-            cur = left;
+        if node.is_some() {
+            1 + max(self.max_depth_from(self.get_left(node)), self.max_depth_from(self.get_right(node)))
         } else {
-            return cur
+            0
         }
     }
-}
 
-fn last(&self) -> I {
-    let mut cur = self.root();
-    
-    loop {
-        let right = self.get_left(cur);
-
-        if right.is_some() {
-            cur = right;
-        } else {
-            return cur
-        }
+    fn max_depth(&self) -> usize {
+        self.max_depth_from(self.root())
     }
-}
 
-fn insert(&mut self, new: I) {
-    let mut node = self.root(); 
-    let new_value = self.value(new);
-
-    loop {
-        let cur_value = self.value(node);
-
-        if new_value > cur_value {
-            let next = self.get_right(node);
-
-            if next.is_none() {
-                self.set_right(node, new);
-                break;
-            } else {
-                node = next;
-            }
-        } else if new_value < cur_value {
-            let next = self.get_left(node);
-
-            if next.is_none() {
-                self.set_left(node, new);
-                break;
-            } else {
-                node = next;
-            }
-        } else {
-            panic!("duplicate item, {:?} is already at {:?} (value {:?})", new_value, node, cur_value);
-        }
+    fn df_iter_from(&'a self, idx: I) -> BTreeDFIter<'a, V, I, P> {
+        BTreeDFIter::new(self, idx)
     }
-}
 
-fn search(&self, search: V) -> Option<I> {
-    let value_to_search = search;
-    let mut current = self.root();
-    let mut next;
-
-    loop {
-        let value = self.value(current);
-
-        if value_to_search < value {
-            next = self.get_left(current);
-        } else if value_to_search > value {
-            next = self.get_right(current);
-        } else {
-            return Some(current);
-        }
-
-        if next.is_some() {
-            current = next;
-        } else {
-            return None;
-        }
+    fn df_iter(&'a self) -> BTreeDFIter<'a, V, I, P> {
+        self.df_iter_from(self.root())
     }
-}
 
-#[cfg(feature = "std")]
-fn dump_dot(&self, filename: &str) -> Result<(), std::io::Error> {
-    use std::io::Write;
-    let mut f = std::fs::File::create(filename).unwrap();
+    fn first(&self) -> I {
+        let mut cur = self.root();
 
-    writeln!(f, "digraph G {{")?;
-    writeln!(f, "rankdir = TB;")?;
-    writeln!(f, "subgraph {{")?;
-    
-    let mut nodes = vec![self.root()];
+        loop {
+            let left = self.get_left(cur);
 
-    while nodes.len() > 0 {
-        let mut new_nodes = Vec::new();
-
-        for node in nodes.clone() {
-            let left = self.get_left(node);
             if left.is_some() {
-                new_nodes.push(left);
-                writeln!(f, "{:?} -> {:?}", self.value(node), self.value(left))?;
+                cur = left;
+            } else {
+                return cur
             }
-
-            let right = self.get_right(node);
-            if right.is_some() {
-                new_nodes.push(right);
-                writeln!(f, "{:?} -> {:?}", self.value(node), self.value(right))?;
-            }
-
-
         }
+    }
 
-        write!(f, "{{ rank = same;")?;
+    fn last(&self) -> I {
+        let mut cur = self.root();
 
-        for node in nodes {
-            write!(f, " {:?};", self.value(node))?;
+        loop {
+            let right = self.get_left(cur);
+
+            if right.is_some() {
+                cur = right;
+            } else {
+                return cur
+            }
+        }
+    }
+
+    fn insert(&mut self, new: I) {
+        let mut node = self.root();
+        let new_value = self.value(new);
+
+        loop {
+            let cur_value = self.value(node);
+
+            if new_value > cur_value {
+                let next = self.get_right(node);
+
+                if next.is_none() {
+                    self.set_right(node, new);
+                    break;
+                } else {
+                    node = next;
+                }
+            } else if new_value < cur_value {
+                let next = self.get_left(node);
+
+                if next.is_none() {
+                    self.set_left(node, new);
+                    break;
+                } else {
+                    node = next;
+                }
+            } else {
+                panic!("duplicate item, {:?} is already at {:?} (value {:?})", new_value, node, cur_value);
+            }
+        }
+    }
+
+    fn search(&self, search: V) -> Option<I> {
+        let value_to_search = search;
+        let mut current = self.root();
+        let mut next;
+
+        loop {
+            let value = self.value(current);
+
+            if value_to_search < value {
+                next = self.get_left(current);
+            } else if value_to_search > value {
+                next = self.get_right(current);
+            } else {
+                return Some(current);
+            }
+
+            if next.is_some() {
+                current = next;
+            } else {
+                return None;
+            }
+        }
+    }
+
+    #[cfg(feature = "std")]
+    fn dump_dot(&self, filename: &str) -> Result<(), std::io::Error> {
+        use std::io::Write;
+        let mut f = std::fs::File::create(filename).unwrap();
+
+        writeln!(f, "digraph G {{")?;
+        writeln!(f, "rankdir = TB;")?;
+        writeln!(f, "subgraph {{")?;
+
+        let mut nodes = vec![self.root()];
+
+        while nodes.len() > 0 {
+            let mut new_nodes = Vec::new();
+
+            for node in nodes.clone() {
+                let left = self.get_left(node);
+                if left.is_some() {
+                    new_nodes.push(left);
+                    writeln!(f, "{:?} -> {:?}", self.value(node), self.value(left))?;
+                }
+
+                let right = self.get_right(node);
+                if right.is_some() {
+                    new_nodes.push(right);
+                    writeln!(f, "{:?} -> {:?}", self.value(node), self.value(right))?;
+                }
+
+
+            }
+
+            write!(f, "{{ rank = same;")?;
+
+            for node in nodes {
+                write!(f, " {:?};", self.value(node))?;
+            }
+
+            writeln!(f, "}}")?;
+
+            nodes = new_nodes;
         }
 
         writeln!(f, "}}")?;
+        writeln!(f, "}}")?;
 
-        nodes = new_nodes;
+        Ok(())
     }
 
-    writeln!(f, "}}")?;
-    writeln!(f, "}}")?;
+    // Algorithm from
+    // Tree Rebalancing in Optimal Time and Space
+    // QUENTIN F. STOUT and BEllE L. WARREN
 
-    Ok(())
-}
+    // TODO(robin): maybe add support for pseudo-root sentinal
+    fn rebalance(&mut self) {
+        let root = self.root();
 
-// Algorithm from 
-// Tree Rebalancing in Optimal Time and Space
-// QUENTIN F. STOUT and BEllE L. WARREN
+        let mut vine_tail = I::none();
+        let mut remainder = root;
+        let mut size = 0;
 
-// TODO(robin): maybe add support for pseudo-root sentinal
-fn rebalance(&mut self) {
-    let root = self.root();
-
-    let mut vine_tail = I::none();
-    let mut remainder = root;
-    let mut size = 0;
-
-    while remainder.is_some() {
-        if self.get_left(remainder).is_none() {
-            vine_tail = remainder;
-            remainder = self.get_right(remainder);
-            size += 1;
-        } else {
-            let tmp = self.get_left(remainder);
-            self.set_left(remainder, self.get_right(tmp));
-            self.set_right(tmp, remainder);
-            remainder = tmp;
-
-            if vine_tail.is_none() {
-                self.set_root(tmp);
+        while remainder.is_some() {
+            if self.get_left(remainder).is_none() {
+                vine_tail = remainder;
+                remainder = self.get_right(remainder);
+                size += 1;
             } else {
-                self.set_right(vine_tail, tmp);
+                let tmp = self.get_left(remainder);
+                self.set_left(remainder, self.get_right(tmp));
+                self.set_right(tmp, remainder);
+                remainder = tmp;
+
+                if vine_tail.is_none() {
+                    self.set_root(tmp);
+                } else {
+                    self.set_right(vine_tail, tmp);
+                }
             }
         }
-    }
 
-    let mut compress = |count| {
-        if count > 0 {
-            let mut child = self.root();
-            let mut child_right = self.get_right(child);
-            self.set_root(child_right);
+        let mut compress = |count| {
+            if count > 0 {
+                let mut child = self.root();
+                let mut child_right = self.get_right(child);
+                self.set_root(child_right);
 
-            let mut scanner = child_right;
-            self.set_right(child, self.get_left(scanner));
-            self.set_left(scanner, child);
-
-
-            for _ in 1..count {
-                child = self.get_right(scanner);
-                child_right = self.get_right(child);
-                self.set_right(scanner, child_right);
-                scanner = child_right;
-
+                let mut scanner = child_right;
                 self.set_right(child, self.get_left(scanner));
                 self.set_left(scanner, child);
+
+
+                for _ in 1..count {
+                    child = self.get_right(scanner);
+                    child_right = self.get_right(child);
+                    self.set_right(scanner, child_right);
+                    scanner = child_right;
+
+                    self.set_right(child, self.get_left(scanner));
+                    self.set_left(scanner, child);
+                }
             }
+        };
+
+        let size_plus_one: u64 = size + 1;
+
+        let leaf_count = if (size_plus_one).is_power_of_two() {
+            0
+        } else {
+            (size_plus_one) - (size_plus_one.next_power_of_two() >> 1)
+        };
+
+        compress(leaf_count);
+        size = size - leaf_count;
+
+        while size > 1 {
+            size >>= 1;
+            compress(size);
         }
-    };
-
-    let size_plus_one: u64 = size + 1;
-
-    let leaf_count = if (size_plus_one).is_power_of_two() {
-        0
-    } else {
-        (size_plus_one) - (size_plus_one.next_power_of_two() >> 1)
-    };
-
-    compress(leaf_count);
-    size = size - leaf_count;
-
-    while size > 1 {
-        size >>= 1;
-        compress(size);
     }
-}
 }
 
 
@@ -480,11 +522,13 @@ mod test {
         ((value + 1).next_power_of_two().trailing_zeros()) as usize
     }
 
-    fn check<V: PartialOrd + Ord + Debug + Clone, I: BTreeIdx + PartialOrd + Ord + Debug, T: BTree<V, I> + Debug>(mut proxy: T, mut actual_values: Vec<(V, I)>) {
+    /*
+    fn check_2<'a, V: PartialOrd + Ord + Debug + Clone, I: BTreeIdx + PartialOrd + Ord + Debug, DFIter: Iterator<Item = (I, V)>, T: BTree<'a, V, I, DFIter> + Debug>(proxy: &'a mut T, mut actual_values: Vec<(V, I)>) {
+
         actual_values.sort_by_key(|ab| ab.0.clone());
         let mut values = Vec::new();
 
-        proxy.df_map(&mut |idx, value| {
+        proxy.df_iter().map(&mut |(idx, value)| {
             values.push((value, idx));
 
             if false {
@@ -493,6 +537,16 @@ mod test {
                 None
             }
         });
+    }
+    */
+
+    fn check<'a>(mut proxy: Proxy<'a>, mut actual_values: Vec<(&OsStr, usize)>) {
+        actual_values.sort_by_key(|ab| ab.0.clone());
+        let mut values = Vec::new();
+
+        for (idx, value) in proxy.df_iter() {
+            values.push((value, idx));
+        }
 
         assert_eq!(actual_values, values);
 
@@ -504,20 +558,15 @@ mod test {
         for (value, idx) in values.iter() {
             assert!(Some(*idx) == proxy.search(value.clone()));
         }
+
         proxy.rebalance();
 
         assert_eq!(proxy.max_depth(), lb(actual_values.len()));
         let mut new_values = Vec::new();
 
-        proxy.df_map(&mut |idx, value| {
+        for (idx, value) in proxy.df_iter() {
             new_values.push((value, idx));
-
-            if false {
-                Some(1)
-            } else {
-                None
-            }
-        });
+        }
 
 
         if !is_sorted(new_values.iter()) {
@@ -531,7 +580,6 @@ mod test {
         for (value, idx) in new_values {
             assert!(Some(idx) == proxy.search(value));
         }
-
     }
 
 
