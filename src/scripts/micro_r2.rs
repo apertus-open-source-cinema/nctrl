0b8567b9ac69220aaa4d1129cc7cb557e1a4e49c
Robin Ole Heinemann <robin.ole.heinemann@t-online.de>
2019-12-03 21:37:08 +0100

    fix micro r2 kick script

--- src/scripts/micro_r2.rs
+++ src/scripts/micro_r2.rs
@@ -7,7 +7,7 @@ script! {
         read => (self) {
             Err(FuseableError::unsupported("read", fuseable::type_name(&self)))
         }
-        write [value] => (self, sensor, sensor_io) {
+        write [value] => (self, ar0330, sensor_io) {
             println!("writing {:?}", value);
 
             sensor_io.write_raw("reset", 1)?;
@@ -15,8 +15,8 @@ script! {
             std::thread::sleep(std::time::Duration::from_millis(10));
 
             sensor_io.write_raw("reset", 0)?;
-            sensor.write_cooked("software_reset", 0)?;
-            sensor.write_cooked("stream", 1)?;
+            ar0330.write_cooked("software_reset", 0)?;
+            ar0330.write_cooked("stream", 1)?;
 
             Ok(())
         }
@@ -29,7 +29,7 @@ script! {
         read => (self) {
             Err(FuseableError::unsupported("read", fuseable::type_name(&self)))
         }
-        write [value] => (self, sensor, sensor_io) {
+        write [value] => (self, ar0330, sensor_io) {
             let extclk = 24000000;
             // init
             // toggle reset (active low)
@@ -40,18 +40,18 @@ script! {
             sensor_io.write_raw("reset", 0x7)?;
 
             // magic init
-            sensor.write_raw("magic_init_config", 0xa114)?;
-            sensor.write_raw("magic_init_start", 0x0070)?;
+            ar0330.write_raw("magic_init_config", 0xa114)?;
+            ar0330.write_raw("magic_init_start", 0x0070)?;
 
             std::thread::sleep(std::time::Duration::from_millis(1));
 
             // check chip_version
-            let chip_version = sensor.read_raw("chip_version_reg")?;
+            let chip_version = ar0330.read_raw("chip_version_reg")?;
             // assert(chip_version == "0x2304");
 
             println!("chip_version {}", chip_version);
-            println!("reserved_chiprev {}", sensor.read_raw("reserved_chiprev")?);
-            println!("version {}", sensor.read_raw("test_data_red")?);
+            println!("reserved_chiprev {}", ar0330.read_raw("reserved_chiprev")?);
+            println!("version {}", ar0330.read_raw("test_data_red")?);
 
             /*
             write("magic_patch1", 0x0146);
@@ -110,33 +110,33 @@ script! {
             let op_sys_clk_div =  2;
             let op_pix_clk_div = 12;
 
-            sensor.write_raw("vt_pix_clk_div", vt_pix_clk_div)?;
-            sensor.write_raw("vt_sys_clk_div", vt_sys_clk_div)?;
-            sensor.write_raw("pre_pll_clk_div", pre_pll_clk_div)?;
-            sensor.write_raw("pll_multiplier", pll_multiplier)?;
-            sensor.write_raw("op_pix_clk_div", op_pix_clk_div)?;
-            sensor.write_raw("op_sys_clk_div", op_sys_clk_div)?;
+            ar0330.write_raw("vt_pix_clk_div", vt_pix_clk_div)?;
+            ar0330.write_raw("vt_sys_clk_div", vt_sys_clk_div)?;
+            ar0330.write_raw("pre_pll_clk_div", pre_pll_clk_div)?;
+            ar0330.write_raw("pll_multiplier", pll_multiplier)?;
+            ar0330.write_raw("op_pix_clk_div", op_pix_clk_div)?;
+            ar0330.write_raw("op_sys_clk_div", op_sys_clk_div)?;
 
             // pll lock time
             std::thread::sleep(std::time::Duration::from_millis(1));
 
             // data format setting
             // 0xc0c - 12bit raw uncompressed
-            sensor.write_raw("data_format_bits", 0x0c0c)?;
+            ar0330.write_raw("data_format_bits", 0x0c0c)?;
             // serial output format
             // select hivcm (1V8)
-            sensor.write_raw("datapath_select", 1 << 9)?;
+            ar0330.write_raw("datapath_select", 1 << 9)?;
 
 
             // hispi enable, test pattern all ones
             // write("hispi_control_status", int("0000 0011 1101 0100".replace(' ', ''), 2))
             // !!!! IMPORTANT  !!!! the 0x0400 bit toggles streaming s -> packetized sp
-            sensor.write_raw("hispi_control_status", 0b1000_0100_0000_0000)?;
-            sensor.write_raw("mipi_config_status", 0xc)?;
+            ar0330.write_raw("hispi_control_status", 0b1000_0100_0000_0000)?;
+            ar0330.write_raw("mipi_config_status", 0xc)?;
 
             // 0x0202 - 2 lane mipi
             // 0x0304 - 4 lane hispi
-            sensor.write_raw("serial_format", 0x0304)?;
+            ar0330.write_raw("serial_format", 0x0304)?;
 
             // test pattern mode
             // 0   - no test pattern
@@ -144,25 +144,25 @@ script! {
             // 2   - solid color bars
             // 3   - fade to gray color bars
             // 256 - walking 1s
-            sensor.write_raw("test_pattern_mode", 0)?;
+            ar0330.write_raw("test_pattern_mode", 0)?;
 
             // unlock write to data_pedestal
-            sensor.write_raw("reset_register", 0b10000)?;
-            sensor.write_raw("test_raw_mode", 2)?;
-            sensor.write_raw("data_pedestal", 0)?;
+            ar0330.write_raw("reset_register", 0b10000)?;
+            ar0330.write_raw("test_raw_mode", 2)?;
+            ar0330.write_raw("data_pedestal", 0)?;
 
             // dubious, we have duplicate addresses for this one
             // sensor.write_register("dark_control", 0)?;
 
-            sensor.write_raw("analog_gain", 0x0010)?;
-            sensor.write_raw("global_gain", 0b0000000010000000)?;
-            sensor.write_raw("coarse_integration_time", 1200)?;
-            sensor.write_raw("fine_integration_time", 0)?;
+            ar0330.write_raw("analog_gain", 0x0010)?;
+            ar0330.write_raw("global_gain", 0b0000000010000000)?;
+            ar0330.write_raw("coarse_integration_time", 1200)?;
+            ar0330.write_raw("fine_integration_time", 0)?;
 
             // reset hispi_timing
-            sensor.write_raw("hispi_timing", 0b1_000_000_000_000_000)?;
+            ar0330.write_raw("hispi_timing", 0b1_000_000_000_000_000)?;
             // streaming enable
-            sensor.write_raw("mode_select", 1)?;
+            ar0330.write_raw("mode_select", 1)?;
 
             Ok(())
         }
